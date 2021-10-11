86783dddfdb3886c8b29221a8e46878ce2afb4c9
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-11 20:51:33 +0200

    Linked js with html

---
+++ build/javascript/index.js
@@ -0,0 +1,9 @@
+const asesor = require("./GoTAsesor");
+const escudero = require("./GoTEscudero");
+const luchador = require("./GoTLuchador");
+const rey = require("./GoTRey");
+const personajes = require("./GoTPersonajes");
+
+const array = [joffrey, jaime, daenerys, tyirion, bronn];
+
+console.log(asesor);
