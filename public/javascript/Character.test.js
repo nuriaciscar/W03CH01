22809da1488bf5ade66da66c332be530eda5e8b2
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-12 21:16:17 +0200

    Html generated Page class

---
+++ public/javascript/Character.test.js
@@ -0,0 +1,20 @@
+import Character from "./Character";
+
+describe("Given a component class", () => {
+  describe("When it receives a parent element, a class name and html tag", () => {
+    test("Then it should render a 'li' element with 'character col' class", () => {
+      //Arrange
+      const parent = document.createElement("ul");
+      const className = "characters";
+      const tag = "li";
+
+      //Act
+      new Component(parent, className, tag);
+      console.log(parent.querySelector("li.characters"));
+      const result = parent.querySelector("li.characters");
+
+      //Assert
+      expect(result).not.toBeNull();
+    });
+  });
+});
